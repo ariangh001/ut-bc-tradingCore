919ac01ccf07508f19209dd99a300305bb375943
Arian <ariangh001@gmail.com>
2023-06-21 18:28:28 +0430

    Add reqs

---
+++ requirements.sh
@@ -0,0 +1,18 @@
+apt-get update && apt-get upgrade -y
+apt-get install npm -y
+npm install pm2 -g
+apt update && apt install sudo curl && curl -sL https://raw.githubusercontent.com/Unitech/pm2/master/packager/setup.deb.sh | sudo -E bash -
+npm install pm2 -g && pm2 update
+apt install python3-pip -y
+apt-get install unzip -y
+apt-get install zip -y
+pip3 install pandas
+pip3 install numpy
+pip3 install bfxapi
+pip3 install bitfinex-tencars
+pip3 install tqdm
+pip3 install python-telegram-bot
+pip3 install bitfinex-api-py
+pip3 install binance-connector
+pip3 install binance-futures-connector
+pip3 install scipy
